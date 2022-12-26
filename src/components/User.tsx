99df513b5fd53f970ddd1b334e20c27c93829a96
Pushon <mh.imranhossain@outlook.com>
2022-12-26 19:54:43 +0600

    update

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -7,14 +7,14 @@ type user = {
 
 export const User = (p: user) => {
   return (
-      <div className='p-5 border-4 rounded-3xl border-slate-500 bg-slate-800'>
-        <h1 className='text-3xl font-black font-serif capitalize text-center'>
-          User info
-        </h1>
-        <strong className='text-amber-300'>Name:</strong>  {p.n} <br />
-        <strong className='text-amber-300'>Age:</strong> {p.a} <br />
-        <strong className='text-amber-300'>Reg:</strong> {p.r ? "True" : "false"} <br />
-        <strong className='text-amber-300'>Lang:</strong> {p.l.map(a => `${a}, `)}
+    <div className='p-5 border-4 rounded-3xl border-slate-500 bg-slate-800'>
+      <h1 className='font-serif text-center'>
+        User info
+      </h1>
+      <strong>Name:</strong>  {p.n} <br />
+      <strong>Age:</strong> {p.a} <br />
+      <strong>Reg:</strong> {p.r ? "True" : "false"} <br />
+      <strong>Lang:</strong> {p.l.map(a => `${a}, `)}
     </div>
   )
 }
