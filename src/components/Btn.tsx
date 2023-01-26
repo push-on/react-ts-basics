52e71f6a25bad43e4f0c90f017f5539d15157f9e
mhimranhossain <mh.imranhossain@outlook.com>
2023-01-26 20:28:05 +0600

    update

--- src/components/Btn.tsx
+++ src/components/Btn.tsx
@@ -1,9 +1,9 @@
 // Sending React Node into props as children
 type btnProps = {
-  children: React.ReactNode
-}
-export const Btn = (props:btnProps) => {
+  children: React.ReactNode;
+};
+export const Btn = (props: btnProps) => {
   return <button className='p-2 border-4 border-blue-500 rounded-xl w-40'>
     {props.children}
-  </button>
-}
+  </button>;
+};
